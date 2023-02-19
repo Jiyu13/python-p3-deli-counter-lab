80a295d02551295351e7c872b1840050f3b1300a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 06:39:12 -0500

    add solutions

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,22 +1,22 @@
 katz_deli = []
 
 def line(katz_deli):
-    if len(katz_deli) == 0:
-        print("The line is currently empty." )
+    if not katz_deli:
+        print("The line is currently empty.")
     else:
-        place = "The line is currently:"
+        message = "The line is currently:"
         for i in range(len(katz_deli)):
-            place += f" {i + 1}. {katz_deli[i]}"
-        print(place)
+            message += f' {i + 1}. {katz_deli[i]}'
+        print(message)
 
-def take_a_number(katz_deli, people):
-    katz_deli.append(people)
-    print(f"Welcome, {people}. You are number {katz_deli.index(people) + 1} in line.")
-
+def take_a_number(katz_deli, new_customer):
+    katz_deli.append(new_customer)
+    print(f'Welcome, {new_customer}. ' +\
+        f'You are number {len(katz_deli)} in line.')
 
 def now_serving(katz_deli):
-    if len(katz_deli) != 0:
-        print(f"Currently serving {katz_deli[0]}.")
-        del katz_deli[0]
-    else:
+    if not katz_deli:
         print("There is nobody waiting to be served!")
+    else:
+        print(f'Currently serving {katz_deli[0]}.')
+        del katz_deli[0]
